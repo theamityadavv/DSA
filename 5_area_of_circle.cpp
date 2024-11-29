de1284a0f8d98038efa2a0b7fba7ace1f0ccfae3
Amit Yadav <133171339+theamityadavv@users.noreply.github.com>
2024-11-29 11:11:23 +0530

    Create 5_area_of_circle.cpp

---
+++ 5_area_of_circle.cpp
@@ -0,0 +1,11 @@
+//5. Calculating the area of the circle
+#include<iostream>
+using namespace std;
+int main(){
+ float radius,area;
+ cout<<"Enter the radius of the Circle \n";
+ cin>>radius;
+ area=3.14*radius*radius;
+ cout<<"The area of the circle is "<<area; 
+ return 0;
+}
