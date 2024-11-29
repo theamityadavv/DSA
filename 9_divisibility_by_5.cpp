7afd27354292dd0db3bf155db95dd45b384aa757
Amit Yadav <133171339+theamityadavv@users.noreply.github.com>
2024-11-29 15:48:52 +0530

    Create 9_divisibility_by_5.cpp

---
+++ 9_divisibility_by_5.cpp
@@ -0,0 +1,16 @@
+//Take positive integer input and tell if it is divisible by 5or not.
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+  cout<<"enter a number \n";
+  cin>>num;
+  if(num%5==0){
+  	cout<<num <<" is divisible by 5\n";
+  }
+  else{
+  	cout<<num <<" is not divisible by 5 \n";
+  }
+}
+
+
