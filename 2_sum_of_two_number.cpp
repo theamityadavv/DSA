77724ececaa6f96aed405bc7f2f07d4623db5674
Amit Yadav <133171339+theamityadavv@users.noreply.github.com>
2024-11-29 11:09:05 +0530

    Create 2_sum_of_two_number.cpp

---
+++ 2_sum_of_two_number.cpp
@@ -0,0 +1,11 @@
+//2.Taking two number as input and then printing their sum
+
+#include<iostream>
+using namespace std;
+int main(){
+	int num1,num2;
+	cout<<"Enter two number \n";
+	cin>>num1 >>num2;
+	cout<<"The sum of the two number " <<num1 <<" And " <<num2  <<" is " <<num1+num2;
+	return 0;	
+}
