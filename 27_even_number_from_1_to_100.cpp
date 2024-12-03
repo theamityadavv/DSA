69a4126b9568e43431f8c735af95033e636e309d
Amit Yadav <133171339+theamityadavv@users.noreply.github.com>
2024-12-03 11:27:23 +0530

    Add files via upload

---
+++ 27_even_number_from_1_to_100.cpp
@@ -0,0 +1,12 @@
+//27. print all even number from 1 to 100
+#include<iostream>
+using namespace std;
+int main(){
+	int i,n;
+	for(i=1;i<=100;i++){
+		if(i%2==0){
+			cout<<i <<endl;
+		}
+	}
+	return 0;
+}
