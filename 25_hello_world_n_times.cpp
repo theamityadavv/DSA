d46d872536983047cfb33381afacdc2820e2b0ef
Amit Yadav <133171339+theamityadavv@users.noreply.github.com>
2024-12-03 11:26:22 +0530

    Add files via upload

---
+++ 25_hello_world_n_times.cpp
@@ -0,0 +1,13 @@
+//25. Print hello world n times. take n as input from the user
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter a number \n";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cout<<"Hello World \n";
+	}
+	return 0;
+}
