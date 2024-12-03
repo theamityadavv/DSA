43c11a16daa16a02977762f3ddf3cd532082255a
Amit Yadav <133171339+theamityadavv@users.noreply.github.com>
2024-12-03 11:29:22 +0530

    Add files via upload

---
+++ 31_Printing_AP_100_to_1.cpp
@@ -0,0 +1,10 @@
+//31. Display this AP- 100,97,94,.... upto all terms which are positive
+
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=100;i>0;i-=3){
+		cout<<i <<", ";
+	}
+	return 0;	
+}
