c66a35185d619ae8e5101877af124458cd39723e
theamityadavv <2k23.cs2314011@gmail.com>
2026-03-31 10:22:53 +0530

    solve pattern 4,5,6

--- 01-Basics/pattern.cpp
+++ 01-Basics/pattern.cpp
@@ -27,6 +27,33 @@ void pattern3(int n) {
             cout<<endl;
         }
     }
+    
+void pattern4(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<i;
+            }
+            cout<<endl;
+        }
+    }
+
+
+void pattern5(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+
+void pattern6(int n) {
+        for(int i=1;i<=n;i++ ){
+            for(int j=1;j<=n-i+1;j++){
+                cout<<j;
+            }
+        }
+    }
 
     int main(){
 
