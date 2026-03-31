02e9d36da8b920409bcf31dfd42e21d2d6aa2a6d
theamityadavv <2k23.cs2314011@gmail.com>
2026-03-31 23:27:46 +0530

    updated

--- 01-Basics/pattern.cpp
+++ 01-Basics/pattern.cpp
@@ -54,9 +54,37 @@ void pattern6(int n) {
             }
         }
     }
-
+void pattern10(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void pattern11(int n){
+    for(int i=1;i<n;i++){
+        for(int j=i;j<n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void pattern12(int n) {
+for(int i=1;i<=2*n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<"*";
+        
+    }
+    for(int j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    cout<<endl;
+}
+    };
     int main(){
-
-
+        int n=5;
+        pattern12(n);
+        
         return 0;
     }
