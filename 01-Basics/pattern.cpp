7b131ad521f736a5f71c0eec121c715cba2a4a09
theamityadavv <2k23.cs2314011@gmail.com>
2026-04-09 11:28:10 +0530

    pattern1

--- 01-Basics/pattern.cpp
+++ 01-Basics/pattern.cpp
@@ -1,90 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void pattern1(int n) {
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                cout<<"*";
-            }
-        cout<<endl;
-        }
-    }
-
-void pattern2(int n) {
-        for(int i = 1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                cout<<"*";
-            }
-            cout<<endl;
-        }
-    }
-
-void pattern3(int n) {
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                cout<<j;
-            }
-            cout<<endl;
-        }
-    }
-    
-void pattern4(int n) {
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                cout<<i;
-            }
-            cout<<endl;
-        }
-    }
-
-
-void pattern5(int n) {
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i+1;j++){
-                cout<<"*";
-            }
-            cout<<endl;
-        }
-    }
-
-void pattern6(int n) {
-        for(int i=1;i<=n;i++ ){
-            for(int j=1;j<=n-i+1;j++){
-                cout<<j;
-            }
-        }
-    }
-void pattern10(int n){
+void pattern1(int n){
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
+        for(int j=1;j<=n;j++){
             cout<<"*";
         }
-        cout<<endl;
-    }
-}
-void pattern11(int n){
-    for(int i=1;i<n;i++){
-        for(int j=i;j<n;j++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-}
-void pattern12(int n) {
-for(int i=1;i<=2*n;i++){
-    for(int j=1;j<=i;j++){
-        cout<<"*";
-        
-    }
-    for(int j=1;j<=n-i;j++){
-        cout<<" ";
-    }
     cout<<endl;
+    }
 }
-    };
+
+
+
     int main(){
         int n=5;
-        pattern12(n);
+        pattern1(n);
         
         return 0;
     }
