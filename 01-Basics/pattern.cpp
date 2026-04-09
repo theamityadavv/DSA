0ac6281d5180ecb1847ec72939c944b0f3e9a2c5
theamityadavv <2k23.cs2314011@gmail.com>
2026-04-09 11:48:34 +0530

    pattern2

--- 01-Basics/pattern.cpp
+++ 01-Basics/pattern.cpp
@@ -10,11 +10,19 @@ void pattern1(int n){
     }
 }
 
+void pattern2(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+}
 
 
     int main(){
         int n=5;
         pattern1(n);
-        
+        pattern2(n);
         return 0;
     }
