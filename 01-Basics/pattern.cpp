6cda514ca826391b25998611764d576c38094e80
theamityadavv <2k23.cs2314011@gmail.com>
2026-04-09 16:04:36 +0530

    completed till pattern 10

--- 01-Basics/pattern.cpp
+++ 01-Basics/pattern.cpp
@@ -9,7 +9,6 @@ void pattern1(int n){
     cout<<endl;
     }
 }
-
 void pattern2(int n){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
@@ -26,13 +25,115 @@ void pattern3(int n){
     cout<<endl;
     }
 }
+void pattern4(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+    cout<<endl;
+    }
+}
+void pattern5(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+}
+void pattern6(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<n-i+1;j++){
+            cout<<j;
+        }
+    cout<<endl;
+    }
+}
+void pattern7(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+}
+void pattern8(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=(2*n-(2*i-1));j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+}
+void pattern9(int n){
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+        }
+          
+    //reverse
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i-1;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*n-(2*i-1);j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+void pattern10(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        cout<<endl;     
+    }
+    // another half
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
 
 
     int main(){
         int n=5;
-        pattern1(n);
-        pattern2(n);
-        pattern3(n);
-
+        // pattern1(n);
+        // pattern2(n);
+        // pattern3(n);
+        // pattern4(n);
+        // pattern5(n);
+        // pattern6(n);
+        // pattern7(n);
+        // pattern8(n);
+        // pattern9(n);
+          pattern10(n);
         return 0;
     }
