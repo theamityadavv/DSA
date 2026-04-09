4f0b3be070c740fe1b3749fea9f7708d41234b37
theamityadavv <2k23.cs2314011@gmail.com>
2026-04-09 11:54:15 +0530

    patten3

--- 01-Basics/pattern.cpp
+++ 01-Basics/pattern.cpp
@@ -18,11 +18,21 @@ void pattern2(int n){
     cout<<endl;
     }
 }
+void pattern3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+    cout<<endl;
+    }
+}
 
 
     int main(){
         int n=5;
         pattern1(n);
         pattern2(n);
+        pattern3(n);
+
         return 0;
     }
